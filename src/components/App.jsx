308f8cc05eeac4b0a6b2f964c541301316b4bb16
Darya Hudzovska <1bestboost@gmail.com>
2022-10-08 19:58:10 +0300

    create card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import { Profile } from 'components/profile/Profile';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile />
     </div>
   );
 };
---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,31 @@
+import css from './Profile.module.css'
+
+export const Profile = ({username, tag, avatar, location, stats}) => {
+    return <div className={css.profile}>
+    <div className={css.description}>
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+        alt="User avatar"
+        className={css.avatar}
+      />
+      <p className={css.username}>Petra Marica</p>
+      <p className={css.tag}>@pmarica</p>
+      <p className={css.location}>Salvador, Brasil</p>
+    </div>
+  
+    <ul className={css.stats}>
+      <li className={css.profileList}>
+        <span className={css.label}>Followers</span>
+        <span className={css.quantity}>1000</span>
+      </li>
+      <li className={css.profileList}>
+        <span className={css.label}>Views</span>
+        <span className={css.quantity}>2000</span>
+      </li>
+      <li className={css.profileList}>
+        <span className={css.label}>Likes</span>
+        <span className={css.quantity}>3000</span>
+      </li>
+    </ul>
+  </div>
+}
