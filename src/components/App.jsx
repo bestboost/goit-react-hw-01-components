e312ec2a4996da1fa520d51e318b96345c757831
Darya Hudzovska <1bestboost@gmail.com>
2022-10-24 21:30:47 +0300

    add props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
 import { Profile } from 'components/profile/Profile';
-import userData from 'user.json';
+import user from '../../src/user.json';
 
 export const App = () => {
   return (
     <div>
-      <Profile users={userData}/>
+      <Profile username={user.username}
+               tag={user.tag}
+               location={user.location}
+               avatar={user.avatar}
+               stats={user.stats}
+               />
     </div>
   );
 };
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,43 +1,44 @@
 import css from './Profile.module.css'
+import PropTypes from 'prop-types';
 
-export const Profile = ({users}) => {
-  console.log(users)
-    return <div className={css.profile}>
-       {users.map(user => ( <p className={css.username} key={user.username}>
-        {user.username}</p>))}|
-    {/* <div className={css.description}>
+export const Profile = ({username, tag, avatar, location, stats}) => {
+    return <div className={css.background}>
+    <div className={css.profile} key={username}>
+    <div className={css.description}>
       <img
-        src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+        src={avatar}
         alt="User avatar"
         className={css.avatar}
       />
-      <p className={css.username}>Petra Marica</p>
-      <p className={css.tag}>@pmarica</p>
-      <p className={css.location}>Salvador, Brasil</p>
+      <p className={css.username}>{username}</p>
+      <p className={css.tag}>{tag}</p>
+      <p className={css.location}>{location}</p>
     </div>
   
     <ul className={css.stats}>
       <li className={css.profileList}>
         <span className={css.label}>Followers</span>
-        <span className={css.quantity}>1000</span>
+        <span className={css.quantity}>{stats.followers}</span>
       </li>
       <li className={css.profileList}>
         <span className={css.label}>Views</span>
-        <span className={css.quantity}>2000</span>
+        <span className={css.quantity}>{stats.views}</span>
       </li>
       <li className={css.profileList}>
         <span className={css.label}>Likes</span>
-        <span className={css.quantity}>3000</span>
+        <span className={css.quantity}>{stats.likes}</span>
       </li>
-    </ul> */}
+     
+    </ul>
+  </div>
   </div>
 }
 
-// Profile.propTypes = {
-//   username: propTypes.string.isRequired,
-//    tag: propTypes.string.isRequired, 
-//    avatar: propTypes.string.isRequired, 
-//    location: propTypes.string.isRequired, 
-//    stats: propTypes.object.isRequired
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+   tag: PropTypes.string.isRequired, 
+   avatar: PropTypes.string.isRequired, 
+   location: PropTypes.string.isRequired, 
+   stats: PropTypes.object.isRequired
 
-// }
+}
